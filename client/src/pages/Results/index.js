00c0fd175a367a974a7067b062e96b0a36a42920
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-16 13:49:52 +0100

    added navLink to results back with play again options

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 function Results (){
-    return(<h1>Results page</h1>)
+    return(<>
+    <h1>Results page</h1>
+    <h1>PLAY AGAIN?</h1>
+    <NavLink to="/setup"><h1>YES</h1></NavLink>
+    <NavLink to="/"><h1>NO</h1></NavLink>
+    </>)
 
 }
 
