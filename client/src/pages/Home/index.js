3f0aec21bcbb01f48907124af996def966030ca8
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-16 13:34:03 +0100

    added Navlink to home page to redirect to other pages

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Home (){
-    return(<h1>Home page</h1>)
+    return(<>
+    <h1>Home page</h1>
+    <NavLink to="setup"><h1>PLAY</h1></NavLink>
+    <h1>JOIN BY ROOM ID</h1>
+    <NavLink to="about"><h1>ABOUT</h1></NavLink>
+    <NavLink to="leaderboard"><h1>LEADERBOARD</h1></NavLink>
+    </>)
 
 }
 
