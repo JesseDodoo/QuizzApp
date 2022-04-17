437d12977b097d91b7f80448ac3b9d6d3fe24a63
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-17 14:32:14 +0100

    added majority of setup form just need actions and submitting form goes to quiz page

--- client/src/pages/Setup/index.js
+++ client/src/pages/Setup/index.js
@@ -1,41 +1,115 @@
 import React, { useState } from 'react';
 import { BackButton } from '../../components'
+import {useNavigate as Navigate} from 'react-router-dom';
+
 
 
 function Setup() {
 
+    const goTo = Navigate();
+
     const [playerNumber, setPlayerNumber] = useState(0);
-    console.log("playerNumber", playerNumber)
+    const [questionNumber, setQuestionNumber] = useState(0);
+    console.log('questionNumber', questionNumber)
 
 
 
-    function playerCount(e){
-        console.log(e.target.value)
+    function playerCount(e) {
         setPlayerNumber(parseInt(e.target.value));
     }
 
-    function renderPlayerInput(){
+    function renderPlayerInput() {
         let inputAreas = [];
-        for(let i = 0; i < playerNumber; i++){
-            inputAreas.push(<input type='text' key={i} placeholder="enter player name..."></input>)
+        for (let i = 0; i < playerNumber; i++) {
+            inputAreas.push(<input type='text' key={i} placeholder="enter player name"></input>)
         }
         return inputAreas;
     }
 
+    function questionCount(e){
+        if(playerNumber !== 0){
+            if(e.target.value % playerNumber !== 0){
+                console.log(`must be a multiple of ${playerNumber}`)
+            }
+            else{
+                setQuestionNumber(parseInt(e.target.value))
+            }
+        }
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        goTo("/quiz")
+    }
+
+
 
     return (<>
         <h1>Setup page</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
+            <label>Local or online?</label>
+            <select name="onlineOrLocal">
+                <option value="">please select</option>
+                <option value="online">Online</option>
+                <option value="local">Local</option>
+            </select>
+            <br></br>
             <label>How many players?</label>
-            <select onChange={playerCount}name="numberOfPlayers">
+            <select onChange={playerCount} name="numberOfPlayers">
                 <option value="">please select</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
             </select>
-
+            <br></br>
             {!playerNumber ? null : renderPlayerInput()}
+            <br></br>
+            <label>Category</label>
+            <select name="trivia_category">
+                <option value="any">Any Category</option>
+                <option value="9">General Knowledge</option>
+                <option value="10">Entertainment: Books</option>
+                <option value="11">Entertainment: Film</option>
+                <option value="12">Entertainment: Music</option>
+                <option value="13">Entertainment: Musicals &amp; Theatres</option>
+                <option value="14">Entertainment: Television</option>
+                <option value="15">Entertainment: Video Games</option>
+                <option value="16">Entertainment: Board Games</option>
+                <option value="17">Science &amp; Nature</option>
+                <option value="18">Science: Computers</option>
+                <option value="19">Science: Mathematics</option>
+                <option value="20">Mythology</option>
+                <option value="21">Sports</option>
+                <option value="22">Geography</option>
+                <option value="23">History</option>
+                <option value="24">Politics</option>
+                <option value="25">Art</option>
+                <option value="26">Celebrities</option>
+                <option value="27">Animals</option>
+                <option value="28">Vehicles</option>
+                <option value="29">Entertainment: Comics</option>
+                <option value="30">Science: Gadgets</option>
+                <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+                <option value="32">Entertainment: Cartoon &amp; Animations</option>
+            </select>
+            <br></br>
+            <select name="trivia_difficulty">
+                <option value="any">Any Difficulty</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+            </select>
+            <br></br>
+            <select name="trivia_type">&gt;
+			<option value="any">Any Type</option>
+			<option value="multiple">Multiple Choice</option>
+			<option value="boolean">True / False</option>
+		</select>
+        <input onChange={questionCount}type='number'></input>
+        <br></br>
+        <button type="submit">PLAY</button>
+
 
         </form>
 
@@ -45,3 +119,6 @@ function Setup() {
 }
 
 export default Setup;
+
+
+
