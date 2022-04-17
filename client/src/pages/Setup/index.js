b97408962d21e6a8e49b159e7376b3c55eb89b24
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-17 13:13:43 +0100

    added how many players input and when selected text inputs are rendered for username

--- client/src/pages/Setup/index.js
+++ client/src/pages/Setup/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BackButton } from '../../components'
 
 
-function Setup (){
-    return(<>
-    <h1>Setup page</h1>
-    <BackButton />
+function Setup() {
+
+    const [playerNumber, setPlayerNumber] = useState(0);
+    console.log("playerNumber", playerNumber)
+
+
+
+    function playerCount(e){
+        console.log(e.target.value)
+        setPlayerNumber(parseInt(e.target.value));
+    }
+
+    function renderPlayerInput(){
+        let inputAreas = [];
+        for(let i = 0; i < playerNumber; i++){
+            inputAreas.push(<input type='text' key={i} placeholder="enter player name..."></input>)
+        }
+        return inputAreas;
+    }
+
+
+    return (<>
+        <h1>Setup page</h1>
+        <form>
+            <label>How many players?</label>
+            <select onChange={playerCount}name="numberOfPlayers">
+                <option value="">please select</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+
+            {!playerNumber ? null : renderPlayerInput()}
+
+        </form>
+
+        <BackButton />
     </>)
 
 }
