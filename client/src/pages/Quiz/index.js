c4b36cbd3f6d7ce5abe050316169abf05bfa5aad
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-19 17:39:52 +0100

    questions are now rendering and change onSubmit

--- client/src/pages/Quiz/index.js
+++ client/src/pages/Quiz/index.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { BackButton } from '../../components'
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate as Navigate } from 'react-router-dom';
 
 function Quiz() {
-
+    const goTo = Navigate();
     const players = useSelector(state => state.players);
-    let quiz = useSelector(state => state.quiz);
-    const [playerNumber, setPlayerNumber] = useState(0);
+    const quiz = useSelector(state => state.quiz);
+    const [playerNumber, setPlayerNumber] = useState(Math.floor(Math.random() * players.length));
     const [playerToAnswer, setPlayerToAnswer] = useState(players[playerNumber].playerName);
+    const [questionNumber, setQuestionNumber] = useState(0);
     console.log("player to answer", playerToAnswer);
     console.log('playerNumber', playerNumber)
     console.log('the players', players);
-    console.log('the quiz', quiz)
+    console.log('the question number', quiz.length)
 
     function renderMultipleChoice() {
 
@@ -27,21 +29,25 @@ function Quiz() {
         if (playerNumber >= (players.length - 1)) {
             setPlayerNumber(0);
             setPlayerToAnswer(players[0].playerName);
+            setQuestionNumber(questionNumber+1)
         }
         else {
             setPlayerNumber(playerNumber + 1);
             setPlayerToAnswer(players[playerNumber + 1].playerName);
+            setQuestionNumber(questionNumber+1)
 
         }
+        if(quiz[questionNumber] === quiz[quiz.length-1]){
+            goTo('/quiz/results');
 
-
-
+        }
     }
 
     return (<>
         <h1>Quiz page</h1>
         <h2>{playerToAnswer}</h2>
         <form onSubmit={onSubmitAnswer}>
+            <h2>question:{!quiz[questionNumber] ? null : quiz[questionNumber].question}</h2>
             <button>ANSWER</button>
         </form>
         <BackButton />
