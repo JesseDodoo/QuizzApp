e04d3180efcdc8ffa40d79ef7f34ce20d3b92117
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-19 14:01:33 +0100

    quiz page renders player name to answer the question name changes on submit

--- client/src/pages/Quiz/index.js
+++ client/src/pages/Quiz/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BackButton } from '../../components'
+import { useSelector, useDispatch } from 'react-redux';
 
-function Quiz (){
-    return(<>
-    <h1>Quiz page</h1>
-    <BackButton />
+function Quiz() {
+
+    const players = useSelector(state => state.players);
+    let quiz = useSelector(state => state.quiz);
+    const [playerNumber, setPlayerNumber] = useState(0);
+    const [playerToAnswer, setPlayerToAnswer] = useState(players[playerNumber].playerName);
+    console.log("player to answer", playerToAnswer);
+    console.log('playerNumber', playerNumber)
+    console.log('the players', players);
+    console.log('the quiz', quiz)
+
+    function renderMultipleChoice() {
+
+    }
+
+    function renderTrueOrFalse() {
+
+    }
+
+    function onSubmitAnswer(e) {
+        e.preventDefault()
+        console.log("you clicked the button")
+        if (playerNumber >= (players.length - 1)) {
+            setPlayerNumber(0);
+            setPlayerToAnswer(players[0].playerName);
+        }
+        else {
+            setPlayerNumber(playerNumber + 1);
+            setPlayerToAnswer(players[playerNumber + 1].playerName);
+
+        }
+
+
+
+    }
+
+    return (<>
+        <h1>Quiz page</h1>
+        <h2>{playerToAnswer}</h2>
+        <form onSubmit={onSubmitAnswer}>
+            <button>ANSWER</button>
+        </form>
+        <BackButton />
     </>)
 
 }
