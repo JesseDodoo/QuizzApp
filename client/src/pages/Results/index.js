1fe66232e34c89b4ce0bd53196cf6b7764595027
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-20 18:01:23 +0100

    results displayed in order on results page

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate as Navigate } from 'react-router-dom';
 import { emptyQuiz } from '../../actions';
 
 
 function Results (){
     const dispatch = useDispatch();
+
+    const players = useSelector(state => state.players);
+
+    function renderScores(){
+        const displayArray = [];
+        players.sort((a, b) => (a.score > b.score ? -1 : 1))
+        const winner = players[0];
+        displayArray.push(<h1>WINNNNNER: {winner.playerName}, score: {winner.score}</h1>)
+        for (let i = 1; i < players.length; i++){
+            displayArray.push(<h2> {i+1} place: {players[i].playerName}, score: {players[i].score}</h2>)
+        }
+
+        return displayArray;
+    }
+
 
     function emptyTheQuiz(){
         dispatch(emptyQuiz())
@@ -17,8 +31,11 @@ function Results (){
 
 
 
+
+
     return(<>
     <h1>Results page</h1>
+    {renderScores()}
     <h1>PLAY AGAIN?</h1>
     <NavLink onClick = {emptyTheQuiz} to="/setup"><h1>YES</h1></NavLink>
     <NavLink onClick = {emptyTheQuiz} to="/"><h1>NO</h1></NavLink>
